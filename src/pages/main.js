26ececd5b3b030c53da7a01474434ddd34eb01dd
skywickenden <github@skywickenden.co.uk>
2018-01-22 18:06:45 +0000

    Importing Matrix libraries

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,19 +1,30 @@
-// import p5 from 'p5';
 import NeuralNetwork from '../js/NeuralNetwork';
+import Matrix from '../js/Matrix';
+import MatrixMath from '../js/MatrixMath';
 
 let brain;
 const sketch = (p) => {
   let gray = 0;
   p.setup = function () {
-    p.createCanvas(600, 400);
-    // console.log('loaded 7');
+    // p.createCanvas(600, 400);
+    console.log('loaded 7');
     brain = new NeuralNetwork(3, 3, 1);
     console.log(brain);
+    let testMatrix = new Matrix(3, 2);
+    let testMatrix2 = new Matrix(3, 2);
+
+    testMatrix = MatrixMath.randomize(testMatrix);
+    testMatrix2 = MatrixMath.randomize(testMatrix2);
+    console.table(testMatrix.data);
+    console.table(testMatrix2.data);
+
+    testMatrix.randomize();
+    console.table(testMatrix.data);
   }
 
   p.draw = function () {
-    p.background(gray);
-    p.rect(p.width/2 - 25, p.height/2 - 25, 50, 50);
+    // p.background(gray);
+    // p.rect(p.width/2 - 25, p.height/2 - 25, 50, 50);
   }
 }
 
